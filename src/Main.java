2ecea5dfcc2c08e8007e04c8dc4e51213d4d67c7
GlakovaA <22109@bk.ru>
2022-07-15 15:39:17 +0300

    Задача 5

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,51 @@
 public class Main {
-    //Задача 4
+    //Задача 5
 
 
-    public static void main(String[] args) { task4(); }
+    public static void main(String[] args) { task5(); }
 
-    private static void task4() {
-        int deliveryDistanse = 95;
-        if (deliveryDistanse <= 20) {
-            System.out.println("Потребуется дней " + 1);
-                if (deliveryDistanse >= 20 && deliveryDistanse <= 60);
-            {
-                System.out.println("Потребуется дней " + 2);
-            }
-            }else if (deliveryDistanse>=60 && deliveryDistanse<=100);{
-                System.out.println("Потребуется дней "+3);}
+    private static void task5() {
+        int monthNumber =5;
+        switch (monthNumber){
+            case 12:
+                System.out.println("декабрь"+" Зима");
+                break;
+            case 1:
+                System.out.println("январь"+" Зима");
+                break;
+            case 2:
+                System.out.println("февраль"+" Зима");
+                break;
+            case 3:
+                System.out.println("март"+" весна");
+                break;
+            case 4:
+                System.out.println("апрель"+" весна");
+                break;
+            case 5:
+                System.out.println("май"+" весна");
+                break;
+            case 6:
+                System.out.println("июнь"+" лето");
+                break;
+            case 7:
+                System.out.println("июль"+" лето");
+                break;
+            case 8:
+                System.out.println("август"+" лето");
+                break;
+            case 9:
+                System.out.println("сентябрь"+" осень");
+                break;
+            case 10:
+                System.out.println("октябрь"+" осень");
+                break;
+            case 11:
+                System.out.println("ноябрь"+" осень");
+                break;
+            default:
+                System.out.println("такого месяца не существует");
+        }
 
 
     }
