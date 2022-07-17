da1c17c85f21fad3d33c50ed6035932590289293
GlakovaA <22109@bk.ru>
2022-07-17 10:23:01 +0300

    Задача 6

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,25 @@
 public class Main {
-    //Задача 5
+    //Задача 6
 
 
-    public static void main(String[] args) { task5(); }
+    public static void main(String[] args) { task6(); }
 
-    private static void task5() {
-        int monthNumber =5;
-        switch (monthNumber){
-            case 12:
-                System.out.println("декабрь"+" Зима");
-                break;
-            case 1:
-                System.out.println("январь"+" Зима");
-                break;
-            case 2:
-                System.out.println("февраль"+" Зима");
-                break;
-            case 3:
-                System.out.println("март"+" весна");
-                break;
-            case 4:
-                System.out.println("апрель"+" весна");
-                break;
-            case 5:
-                System.out.println("май"+" весна");
-                break;
-            case 6:
-                System.out.println("июнь"+" лето");
-                break;
-            case 7:
-                System.out.println("июль"+" лето");
-                break;
-            case 8:
-                System.out.println("август"+" лето");
-                break;
-            case 9:
-                System.out.println("сентябрь"+" осень");
-                break;
-            case 10:
-                System.out.println("октябрь"+" осень");
-                break;
-            case 11:
-                System.out.println("ноябрь"+" осень");
-                break;
-            default:
-                System.out.println("такого месяца не существует");
-        }
+    private static void task6() {
+       int age = 19;
+       int salary = 58_000;
+       double x=1.5;
+       int y=2;
+       int z=3;
+       double h=1.2;
 
+       if (age>=23 && salary>=50_000) {
+                System.out.println("Мы готовы выдать Вам кредитную карту с лимитом "+ salary*y*h);}
+              if (age>=23 && salary>=80_000){
+                System.out.println("Мы готовы выдать Вам кредитную карту с лимитом "+ salary*z*x);}
+              if (age<=23 && salary>=50_000){
+                System.out.println("Мы готовы выдать Вам кредитную карту с лимитом "+ salary*y*h);}
+              if(age<=23 && salary>=80_000){
+                System.out.println("Мы готовы выдать Вам кредитную карту с лимитом "+ salary*y*x);}
 
     }
 }
